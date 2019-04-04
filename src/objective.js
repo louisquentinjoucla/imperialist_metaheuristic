8b50e0b20b527537b1ce83351bc6a1ec590842d0
lowlighter <simon.lecoq@live.fr>
2019-04-04 02:17:58 -0400

    Update objective.js

--- src/objective.js
+++ src/objective.js
@@ -20,9 +20,20 @@ function booth(x,y){
     return (x + 2*y - 7)**2 + (2*x + y -5)**2
 }
 
+function noise_simplex(x, y) {
+    return noise.simplex2(x/50, y/50)
+}
+
+function noise_perlin(x, y) {
+    return noise.perlin2(x/50, y/50)
+}
+
+
 let test_functions = [
     {name:'Eggholder', func:eggholder, limit:[[-100, 100],[-100, 100]]},
     {name:'Himmelbleau', func:himmelblau, limit:[[-100, 100],[-100, 100]]},
     {name:'Beale ', func:beale, limit:[[-4.5, 4.5],[-4.5, 4.5]]},
     {name:'Booth', func:booth, limit:[[-10, 10],[-10, 10]]},
+    {name:'Noise (Simplex)', func:noise_simplex, limit:[[-100, 100],[-100, 100]]},
+    {name:'Noise (Perlin)', func:noise_perlin, limit:[[-100, 100],[-100, 100]]},
 ]
